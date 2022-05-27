ca452e4d05016fe5e52ffa69807d85cb12f282e6
Vishal Ravanank <37749615+FL45h-09@users.noreply.github.com>
2022-05-27 19:44:46 +0530

    Add files via upload

---
+++ js/carousel-transition.js
@@ -0,0 +1,22 @@
+$('.gatawayslider').owlCarousel({
+    autoplay: 100,
+    autoplayHoverPause: true,
+    slideSpeed: 100,
+    paginationSpeed: 200,
+    loop: true,
+    margin: 30,
+    nav: true,
+    dots: false,
+    items: 3
+})
+
+$('.captionslider').owlCarousel({
+    autoplay: 50,
+    slideSpeed: 1000,
+    paginationSpeed: 1000,
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    items: 1
+})
